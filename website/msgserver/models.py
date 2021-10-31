a7243b3227e8aa592ca86f470a083c54214bc505
sorochak <austensorochak@gmail.com>
2021-10-31 11:32:48 -0700

    Lab 5 completion

--- website/msgserver/models.py
+++ website/msgserver/models.py
@@ -2,25 +2,76 @@ from django.db import models
 import msgserver.constants
 from django.core.exceptions import ValidationError
 
-# Create your models here.
+#
+# PURPOSE:
+# Validates to ensure a key is alphanumeric and length of key equals KEYLENGTH
+#
+# PARAMETERS:
+# 'key' contains the key field of a Message instance
+#
+# RETURN/SIDE EFFECTS:
+# Raises a validation error if key is not alphanumeric or does not equal KEYLENGTH.
+#
 
 def validate_key(key):
     if len(key) != msgserver.constants.KEYLENGTH or not key.isalnum():
         raise ValidationError('Invalid Key', code='key_value')
 
+#
+# PURPOSE:
+# Validates to ensure a message is greater than or equal to MINMSGLENGTH or less than or equal to MAXMSGLENGTH
+#
+# PARAMETERS:
+# 'msg' contains the message field of a Message instance
+#
+# RETURN/SIDE EFFECTS:
+# Raises a validation error if msg is less than MINMSGLENGTH or  greater than MAXMSGLENGTH.
+#
+
 def validate_msg_length(msg):
     if len(msg) < msgserver.constants.MINMSGLENGTH or len(msg) > msgserver.constants.MAXMSGLENGTH:
         raise ValidationError('Invalid Message Length', code='message_value')
+
+#
+# PURPOSE:
+# Validates to ensure a key is not duplicated
+#
+# PARAMETERS:
+# 'k' contains the key field of a Message instance
+#
+# RETURN/SIDE EFFECTS:
+# Raises a validation error if k is found to duplicate a key already in the database.
+#
 
 def validate_unique_key(k):
     for message in Message.objects.all():
         if message.key == k:
             raise ValidationError('Key taken', code='duplicate key')
 
+#
+# PURPOSE:
+# Defines a Message model
+#
+# PARAMETERS:
+# 'models.Model' contains Django's Model class
+#
+# RETURN/SIDE EFFECTS:
+# Creates a Message instance in the database.
+#
 
 class Message(models.Model):
     key = models.CharField(max_length= msgserver.constants.KEYLENGTH, validators=[validate_key, validate_unique_key])
     message = models.CharField(max_length= msgserver.constants.MAXMSGLENGTH, validators=[validate_msg_length])
 
+    #
+    # PURPOSE:
+    # Returns the string representation of a Message instance
+    #
+    # PARAMETERS:
+    # 'self' contains the Message instance.
+    #
+    # RETURN/SIDE EFFECTS:
+    # Returns the string representation of the Message instance 'self'.
+    #
     def __str__(self):
         return self.key + ' : ' + self.message
--- website/msgserver/views.py
+++ website/msgserver/views.py
@@ -4,28 +4,91 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 import json
 
+#
+# PURPOSE:
+# Filters the database for a specific key and returns a Web Response that contains the key and associated message.
+#
+# PARAMETERS:
+# 'request' contains a Web Request
+# 'key' contains an alphanumeric key used to identify a message.
+#
+# RETURN/SIDE EFFECTS:
+# Returns a Web Response that contains a key and associated message.
+#
 
 def get_message(request,key):
     message = Message.objects.filter(key=key)
     return HttpResponse("%(key)s: %(message)s" % { 'key':message[0].key, 'message':message[0].message})
 
+#
+# PURPOSE:
+# Returns a Web Response that contains all messages in JSON format.
+#
+# PARAMETERS:
+# 'request' contains a Web Request.
+#
+# RETURN/SIDE EFFECTS:
+# Returns a Web Response that contains all messages in JSON format.
+#
+
 def index(request):
     return HttpResponse(json.dumps(list(Message.objects.all()), cls=MessageEncoder))
 
+#
+# PURPOSE:
+# A class based generic view for a form to complete a database insertion
+#
+# PARAMETERS:
+# 'CreateView' contains a view that displays a form for creating a Message object.
+#
+# RETURN/SIDE EFFECTS:
+# Upon successful database insertion, calls the index view.
+#
 
 class MessageCreate(CreateView):
     model = Message
     fields = '__all__'
     success_url = reverse_lazy('index')
 
+#
+# PURPOSE:
+# A class based generic view for a form to update a previous database insertion
+#
+# PARAMETERS:
+# 'UpdateView' contains a view that updates an instance of a Message object.
+#
+# RETURN/SIDE EFFECTS:
+# Upon successful object update, calls the index view.
+#
+
 class MessageUpdate(UpdateView):
     model = Message
     fields = ['message']
     success_url = reverse_lazy('index')
 
+    #
+    # PURPOSE:
+    # override of Django's get_object view, this method retrieves the object
+    #
+    # PARAMETERS:
+    # 'self' contains the object being created.
+    #
+    # RETURN/SIDE EFFECTS:
+    # returns the object associated with a key.
+    #
     def get_object(self):
         return Message.objects.get(key=self.kwargs.get("key"))
-   
+
+#
+# PURPOSE:
+# A class based view for encoding classes to JSON format
+#
+# PARAMETERS:
+# 'json.JSONEncoder' contains an extensible JSON encoder for Python data structures.
+#
+# RETURN/SIDE EFFECTS:
+# calls a function that returns an instance of a Message encoded to JSON.
+#
 
 class MessageEncoder(json.JSONEncoder):
     def default(self, obj):
