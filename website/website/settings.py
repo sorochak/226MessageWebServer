7919d7d23d75afa69744d3a1ffeb3a6a1b4a8c10
sorochak <austensorochak@gmail.com>
2021-11-19 10:25:09 -0800

    fixed run test error

--- website/website/settings.py
+++ website/website/settings.py
@@ -140,4 +140,4 @@ if os.environ.get('DJANGO_SECRET_KEY') != 'TESTING':
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-django_heroku.settings(locals())
+django_heroku.settings(locals(),test_runner=False)
