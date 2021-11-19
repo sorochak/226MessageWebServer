1edc24cab96d25739133f1be2d9462ca22c14336
sorochak <austensorochak@gmail.com>
2021-11-19 11:13:35 -0800

    debug to false

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rocky-hollows-71947.herokuapp.com', '127.0.0.1']
 
