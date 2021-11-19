8814a9c5dff5c4eeba1d7cd408fe1cc15218f8cb
sorochak <austensorochak@gmail.com>
2021-11-19 11:03:30 -0800

    deployed to heroku

--- website/website/settings.py
+++ website/website/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +28,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rocky-hollows-71947.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
